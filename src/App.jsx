b18bb8fa77c96a846c7264b2313094d9867faf21
kdt117 <kdt117@txstate.edu>
2023-09-25 00:09:50 +0700

    fixed styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import About from "./components/about/About"
-import Education from "./components/education/Education"
-import Experience from './components/experience/Experience'
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
-import Nav from "./components/nav/Nav"
-import Project from './components/projects/Project'
-import Contact from "./components/contact/Contact"
-
+import React from "react";
+import About from "./components/about/About";
+import Education from "./components/education/Education";
+import Experience from "./components/experience/Experience";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Nav from "./components/nav/Nav";
+import Project from "./components/projects/Project";
+import Contact from "./components/contact/Contact";
+import { Route, Router, Routes } from "react-router-dom";
+import MyResume from "./components/MyResume/MyResume";
 const App = () => {
   return (
     <div>
-        <Header/>
-        <Nav/>
-        <About/>
-        <Experience/>
-        <Project/>  
-        <Contact/>
-        {/* <Education/> */}
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Project />
+      <Contact />
+      {/* <Education/> */}
 
-        <Footer/>
+      <Footer />
+      <Routes>
+        <Route path="/resume" Component={MyResume} />
+      </Routes>
     </div>
-    
-
-  )
-}
+  );
+};
 
 export default App;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,59 +1,55 @@
-import React from 'react'
-import {BsPatchCheckFill} from "react-icons/bs"
-import "./experience.css"
+import React from "react";
+import { BsPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 const Experience = () => {
   return (
     <section id="experience">
-        <h5>Skills I have</h5>
-        <h2>My Experience</h2>
-        <div className='container experience__container'>
-            <div className='experience__frontend'>
-              <h3>Frontend Development</h3>
-              <div className='experience__content'>
-                <article className='experience__details'>
-                      <BsPatchCheckFill className="experience__details-icon"/>
-                      <h4>HTML</h4>
-                  </article>
-                  <article className='experience__details'>
-                      <BsPatchCheckFill className="experience__details-icon"/>
-                      <h4>CSS</h4>
-                  </article>
-                  <article className='experience__details'>
-                      <BsPatchCheckFill className="experience__details-icon"/>
-                      <h4>Javascript</h4>
-                  </article>
-                  <article className='experience__details'>
-                      <BsPatchCheckFill className="experience__details-icon"/>
-                      <h4>ReactJS</h4>
-                  </article>
-              </div>
-            </div>
+      <h5>Skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>HTML</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>CSS</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>Javascript</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>ReactJS</h5>
+            </article>
+          </div>
+        </div>
 
-            <div className='experience__backend'>
-              <h3>Backend Development</h3>
-              <div className='experience__content'>
-                <article className='experience__details'>
-                    <BsPatchCheckFill className="experience__details-icon"/>
-                    <h4>Node.js</h4>
-                </article>
-                <article className='experience__details'>
-                    <BsPatchCheckFill className="experience__details-icon"/>
-                    <h4>Express</h4>
-                </article>
-                <article className='experience__details'>
-                    <BsPatchCheckFill className="experience__details-icon"/>
-                    <h4>MongoDB</h4>
-                </article>
-                <article className='experience__details'>
-                    <BsPatchCheckFill className="experience__details-icon"/>
-                    <h4>Python</h4>
-                </article>
-                
-                
-                
-
-            </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>Node.js</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>Express</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>MongoDB</h5>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <h5>Python</h5>
+            </article>
+          </div>
         </div>
 
         {/* <div className='experience__frontend'>
@@ -99,9 +95,9 @@ const Experience = () => {
                   </article>
               </div>
             </div> */}
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
